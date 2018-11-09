47f90c32c4f2e85546d6ac7319c410352e891615
Axolotl <samuelmachat@gmail.com>
2018-11-09 12:56:23 -0500

    configure CRUD routes for actions and projects

--- server/routes/actionsRouter.js
+++ server/routes/actionsRouter.js
@@ -3,4 +3,91 @@ const db = require('../../data/helpers/actionModel.js')
 
 const router = express.Router()
 
+router.get('/', async (req, res) => {
+  try {
+    const actions = await db.get()
+    if (actions) {
+      res.status(200).json(actions)
+    } else {
+      res.status(404).json({ error: 'no actions found' })
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'faild to retrieve actions' })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const action = await db.get(id)
+    if (action) {
+      res.status(200).json(action)
+    } else {
+      res.status(404).json({ error: `action ${id} not found` })
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'faild to retrieve action' })
+  }
+})
+
+router.post('/', async (req, res) => {
+  const { project_id, description, notes } = req.body
+
+  if (!project_id || !description || !notes) {
+    res.status(400).json({ error: 'missing data for action' })
+  } else {
+    try {
+      const newActionId = await db.insert(req.body)
+      const newAction = await db.get(newActionId)
+      res.status(201).json(newAction)
+    } catch (error) {
+      res.status(500).json({
+        error: 'failed to save action to db'
+      })
+    }
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  const { project_id, description, notes, completed } = req.body
+
+  if (!db.get(req.params.id)) {
+    res.status(404).json({ error: 'no action with that id' })
+  } else if (!project_id && !description && !notes && !completed) {
+    res.status(400).json({ error: 'no data to upldate action provided' })
+  } else {
+    try {
+      const count = await db.update(req.params.id, req.body)
+      if (count) {
+        const action = await db.get(req.params.id)
+        res.status(200).json(action)
+      } else {
+        res.status(400).json({ error: 'failed to update action in db' })
+      }
+    } catch (error) {
+      res.status(500).json({ error: 'db error' })
+    }
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  const action = await db.get(req.params.id)
+
+  if (!action) {
+    res.status(404).json({ error: 'action to delete not found' })
+  } else {
+    try {
+      const count = await db.remove(req.params.id)
+      if (count) {
+        res.status(200).json(action)
+      } else {
+        res.status(400).json({ error: 'error deleting action' })
+      }
+    } catch (error) {
+      res.status(500).json({ error: 'db error' })
+    }
+  }
+})
+
 module.exports = router
--- server/routes/projectsRouter.js
+++ server/routes/projectsRouter.js
@@ -3,4 +3,106 @@ const db = require('../../data/helpers/projectModel.js')
 
 const router = express.Router()
 
+router.get('/', async (req, res) => {
+  try {
+    const projects = await db.get()
+    if (projects) {
+      res.status(200).json(projects)
+    } else {
+      res.status(404).json({ error: 'no projects found' })
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'faild to retrieve projects' })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const project = await db.get(id)
+    if (project) {
+      res.status(200).json(project)
+    } else {
+      res.status(404).json({ error: `project ${id} not found` })
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'faild to retrieve project' })
+  }
+})
+
+router.get('/:id/posts', async (req, res) => {
+  try {
+    const actions = await db.getProjectActions(req.params.id)
+    if (actions) {
+      res.status(200).json(actions)
+    } else {
+      res.status(404).json({ error: 'no actions found for that project' })
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'db error' })
+  }
+})
+
+router.post('/', async (req, res) => {
+  const { name, description } = req.body
+
+  if (!name || !description) {
+    res
+      .status(400)
+      .json({ error: 'make sure you include name and description' })
+  } else {
+    try {
+      const newProjectId = await db.insert(req.body)
+      const newProject = await db.get(newProjectId)
+      res.status(201).json(newProject)
+    } catch (error) {
+      res.status(500).json({
+        error: 'failed to save project to db'
+      })
+    }
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  const { name, description, completed } = req.body
+
+  if (!db.get(req.params.id)) {
+    res.status(404).json({ error: 'no project with that id' })
+  } else if (!name && !description && !completed) {
+    res.status(400).json({ error: 'no update information provided' })
+  } else {
+    try {
+      const count = await db.update(req.params.id, req.body)
+      if (count) {
+        const project = await db.get(req.params.id)
+        res.status(200).json(project)
+      } else {
+        res.status(400).json({ error: 'failed to update project in db' })
+      }
+    } catch (error) {
+      res.status(500).json({ error: 'db error' })
+    }
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  const project = await db.get(req.params.id)
+
+  if (!project) {
+    res.status(404).json({ error: 'project to delete not found' })
+  } else {
+    try {
+      const count = await db.remove(req.params.id)
+      if (count) {
+        res.status(200).json(project)
+      } else {
+        res.status(400).json({ error: 'error deleting project' })
+      }
+    } catch (error) {
+      res.status(500).json({ error: 'db error' })
+    }
+  }
+})
+
 module.exports = router
